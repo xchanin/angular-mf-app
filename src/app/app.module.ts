10f1ec21d8c16db878fad3f737b2830f4b2252e5
xchanin <shannonwbruns@gmail.com>
2022-10-03 10:13:43 -0600

    trying

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CComponent } from './c/c.component';
 @NgModule({
   imports: [
     BrowserModule,
+    ATestModule,
     RouterModule.forRoot([
       { path: 'angulartest/a', component: AComponent },
       { path: 'angulartest/b', component: BComponent },
