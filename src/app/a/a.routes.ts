bd7bd9474b14bd99b5082c8cdd2196d687b20d07
xchanin <shannonwbruns@gmail.com>
2022-10-03 09:35:21 -0600

    add a module

---
+++ src/app/a/a.routes.ts
@@ -0,0 +1,9 @@
+import { AComponent } from './a.component';
+import { Routes } from '@angular/router';
+
+export const A_ROUTES: Routes = [
+    {
+        path: 'a-route',
+        component: AComponent
+    }
+];
